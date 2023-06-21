6538a560ea11d42ea087a27ea7519a684562cf14
Hanna922 <hanna.urssu@gmail.com>
2023-06-21 14:51:36 +0900

    chore: config eslint, prettier, .vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,55 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'import'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    semi: ['error', 'never'],
+    'no-console': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', 'react-dom'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+      },
+    ],
+  },
+}
