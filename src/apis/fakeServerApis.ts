1c6c4269edf3c0cc439fbfb543a38fd15cba1707
Emily <119949731+EmilyPPP@users.noreply.github.com>
2023-06-27 00:44:04 +0900

    feat: add fake apis

---
+++ src/apis/fakeServerApis.ts
@@ -0,0 +1,13 @@
+import contentImage from '@/data/contentImage.json'
+import contentLink from '@/data/contentLink.json'
+import { Content } from '@/models/Content'
+import { ContentType } from '@/models/enums/ContentType'
+
+/**
+ * 콘텐츠를 가져옵니다.
+ */
+export const getContent = async (id: string, type: ContentType): Promise<Content | undefined> => {
+  const json = JSON.stringify(type === ContentType.Image ? contentImage : contentLink)
+  const result: Content = JSON.parse(json)
+  return result
+}
