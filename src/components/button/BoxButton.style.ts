7ad29f29f5e11e15b687df98c13d9de0c8d1735e
Hanna922 <hanna.urssu@gmail.com>
2023-07-01 18:54:47 +0900

    feat: add BoxButton
    - BoxButton을 재사용할 수 있도록 구현했습니다.

---
+++ src/components/button/BoxButton.style.ts
@@ -0,0 +1,30 @@
+import styled, { css } from '@emotion/native'
+
+import { colors } from '@/styles/colors'
+
+/**
+ *
+ */
+
+export const BoxButtonContainer = styled.TouchableHighlight`
+  justify-content: center;
+  align-items: center;
+  width: 325px;
+  height: 56px;
+  border-radius: 8px;
+`
+
+export const BoxButtonStyles = {
+  default: css`
+    background-color: ${colors.mainYelllow};
+    color: ${colors.gray500};
+  `,
+  disabled: css`
+    background-color: ${colors.gray50};
+    color: ${colors.gray300};
+  `,
+  hover: css`
+    background-color: ${colors.yellow500};
+    color: ${colors.gray500};
+  `,
+}
---
+++ src/components/button/BoxButton.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+import { Text } from 'react-native'
+
+import { colors } from '@/styles/colors'
+
+import { BoxButtonContainer, BoxButtonStyles } from './BoxButton.style'
+
+/**
+ *
+ */
+
+interface BoxButtonProps {
+  text: string
+  onPress: () => void
+  buttonStyle: 'default' | 'disabled'
+}
+
+/**
+ *
+ */
+export const BoxButton = (props: BoxButtonProps) => {
+  const { text, onPress, buttonStyle } = props
+  const [isPressed, setIsPressed] = useState(false)
+
+  /**
+   *
+   */
+  const handlePressIn = () => {
+    setIsPressed(true)
+  }
+
+  /**
+   *
+   */
+  const handlePressOut = () => {
+    setIsPressed(false)
+  }
+
+  /**
+   *
+   */
+  const handleButtonStyle = () => {
+    if (isPressed && buttonStyle === 'default') {
+      return BoxButtonStyles.hover
+    } else if (buttonStyle === 'disabled') {
+      return BoxButtonStyles.disabled
+    } else {
+      return BoxButtonStyles.default
+    }
+  }
+
+  return (
+    <BoxButtonContainer
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      style={handleButtonStyle()}
+      onPress={onPress}
+      underlayColor={colors.yellow500}
+    >
+      <Text style={handleButtonStyle()}>{text}</Text>
+    </BoxButtonContainer>
+  )
+}
