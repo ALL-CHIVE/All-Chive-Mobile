74306776eb92939ef1de7ebac4dfef16e6f23190
Hanna922 <hanna.urssu@gmail.com>
2023-07-05 02:01:41 +0900

    feat: Archiving.tsx 추가 구현

--- src/screens/archiving/Archiving.style.ts
+++ src/screens/archiving/Archiving.style.ts
@@ -7,13 +7,31 @@ export const ArchivingContainer = styled.View`
   padding: 0 25px;
 `
 
+export const SearchBar = styled.TouchableOpacity`
+  width: 300px;
+  height: 34px;
+  border-radius: 19.5px;
+  background-color: ${colors.white};
+  margin-top: 27px;
+  /* 돋보기 아이콘 추가 */
+`
+
 export const NicknameText = styled.Text`
   font-family: ${fonts.subtitle1};
   color: ${colors.gray600};
+  margin-top: 28px;
 `
 
 export const TitleText = styled.Text`
   font-family: ${fonts.heading2};
   color: ${colors.gray600};
-  white-space: pre-line;
+`
+
+export const CategoryContainer = styled.View`
+  flex-direction: row;
+  margin-top: 180px;
+`
+
+export const CategoryListContainer = styled.View`
+  /* TODO */
 `
--- src/screens/archiving/Archiving.tsx
+++ src/screens/archiving/Archiving.tsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ScrollView, View } from 'react-native'
-import { SearchBar } from 'react-native-screens'
+import { AxiosError } from 'axios'
+import { ScrollView, TouchableOpacity } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+import { useQuery } from 'react-query'
 
-import { BoxButton } from '@/components/button/BoxButton'
-import { Category } from '@/components/category/Category'
+import { CategoryList } from '@/components/list/CategoryList'
+import { Topic } from '@/components/topic/Topic'
+import { PopupMenu } from '@/models/PopupMenu'
+import { ArchivingCategoryList } from '@/models/category/CategoryList'
+import { colors } from '@/styles/colors'
 
-import { ArchivingContainer, NicknameText, TitleText } from './Archiving.style'
+import {
+  ArchivingContainer,
+  CategoryContainer,
+  CategoryListContainer,
+  NicknameText,
+  SearchBar,
+  TitleText,
+} from './Archiving.style'
+import { getCategoryList } from './apis/getCategoryList'
+
+// 추후 수정
+const TopicList = [
+  '전체',
+  '푸드',
+  '라이프',
+  '홈·리빙',
+  '쇼핑',
+  '스포츠',
+  '자기계발',
+  '테크',
+  '디자인',
+  '트렌드',
+]
 
 /**
  *
  */
 export const Archiving = () => {
+  const { data: categoryList } = useQuery<ArchivingCategoryList, AxiosError>(
+    ['getCategoryList'],
+    () => getCategoryList()
+  )
+
   /**
    *
    */
@@ -19,34 +51,70 @@ export const Archiving = () => {
     // console.log('upload') // upload 화면으로 navigation
   }
 
+  /**
+   *
+   */
+  const handleClickTopic = () => {
+    // TODO
+  }
+
   return (
     <>
       <ScrollView>
-        <ArchivingContainer>
-          <SearchBar />
-          <NicknameText>다카이브님</NicknameText>
-          <TitleText>{`현재까지 총 10개의\n아카이빙을\n저장하고 계세요!`}</TitleText>
-          <View style={{ flexDirection: 'row' }}>
-            <Category
-              text="푸드"
-              onPress={handleUplaod}
-            />
-            <Category
-              text="패션"
-              onPress={handleUplaod}
-            />
-            <Category
-              text="패션 테스트테스트 테스트"
-              onPress={handleUplaod}
-            />
-          </View>
-          <BoxButton
-            text="default"
-            onPress={handleUplaod}
-            isDisabled={false}
-          />
-        </ArchivingContainer>
+        <LinearGradient colors={[colors.yellow200, colors.white]}>
+          <ArchivingContainer>
+            <SearchBar />
+            {/* Profile Button */}
+            <NicknameText>다카이브님</NicknameText>
+            <TitleText>{`현재까지 총 10개의\n아카이빙을\n저장하고 계세요!`}</TitleText>
+            <ScrollView horizontal={true}>
+              <CategoryContainer style={{ flexDirection: 'row' }}>
+                {TopicList.map((category, index) => (
+                  <Topic
+                    key={index}
+                    text={category}
+                    onPress={handleClickTopic} // 추후 수정
+                  />
+                ))}
+              </CategoryContainer>
+            </ScrollView>
+            <CategoryListContainer>
+              <TouchableOpacity>
+                <CategoryList
+                  title="흑백 타이포 그래피 래퍼 아카이빙 흑백 타이포 그래피 래퍼 아카이빙"
+                  day="2022.10.27"
+                  popupMenuList={PopupMenuList}
+                  imgCnt={1}
+                  linkCnt={2}
+                  scrapCnt={3}
+                />
+              </TouchableOpacity>
+            </CategoryListContainer>
+          </ArchivingContainer>
+        </LinearGradient>
       </ScrollView>
     </>
   )
 }
+
+/**
+ *
+ */
+const HandleFix = () => {
+  // TODO
+}
+
+/**
+ *
+ */
+const HandleRemove = () => {
+  // TODO
+}
+
+const PopupMenuList: PopupMenu[] = [
+  {
+    title: '고정',
+    onClick: HandleFix,
+  },
+  { title: '삭제', onClick: HandleRemove },
+]
