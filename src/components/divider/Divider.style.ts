f173545ce51ea2a60de99d0592e83707f918f251
Hanna922 <hanna.urssu@gmail.com>
2023-07-16 16:00:45 +0900

    feat: ArchivingModal 수정, Divider 추가, ko.json 수정

---
+++ src/components/divider/Divider.style.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/native'
+
+import { colors } from '@/styles/colors'
+
+export const Container = styled.View`
+  height: 1;
+  width: 100%;
+  background-color: ${colors.gray100};
+  margin: 10px 0px;
+`
