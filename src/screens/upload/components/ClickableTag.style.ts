17288718374fe03588022e955a096f8340889948
Hanna922 <hanna.urssu@gmail.com>
2023-07-17 22:34:26 +0900

    feat: add ClickableTag

---
+++ src/screens/upload/components/ClickableTag.style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/native'
+
+import { colors } from '@/styles/colors'
+
+export const Container = styled.TouchableOpacity`
+  background-color: ${colors.white};
+  padding: 10px;
+  border-radius: 7px;
+  margin-top: 10px;
+  margin-right: 7px;
+`
+
+export const Text = styled.Text`
+  color: ${colors.gray300};
+`
