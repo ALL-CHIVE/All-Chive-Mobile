ba637346cc045fb75e7efaede1b96ed99491252d
Emily <119949731+EmilyPPP@users.noreply.github.com>
2023-08-11 21:39:49 +0900

    feat: update block apis

--- src/screens/contentList/ContentList.tsx
+++ src/screens/contentList/ContentList.tsx
@@ -7,6 +7,7 @@ import { ListRenderItem, NativeScrollEvent } from 'react-native'
 import { useInfiniteQuery, useMutation, useQueryClient } from 'react-query'
 
 import { deleteArchiving, getContentByArchiving } from '@/apis/archiving'
+import { postBlock } from '@/apis/block'
 import { defaultImages } from '@/assets'
 import ContentCard from '@/components/cards/contentCard/ContentCard'
 import DefaultContainer from '@/components/containers/defaultContainer/DefaultContainer'
@@ -43,6 +44,7 @@ const ContentList = ({ route }: ContentListProps) => {
   const [isDeleteDialogVisible, setIsDeleteDialogVisible] = useState(false)
   const [isBlockDialogVisible, setIsBlockDialogVisible] = useState(false)
   const [isBlockCompleteDialogVisible, setIsBlockCompleteDialogVisible] = useState(false)
+  const [ownerNickname, setOwnerNickname] = useState('')
   const queryClient = useQueryClient()
 
   const {
@@ -77,6 +79,25 @@ const ContentList = ({ route }: ContentListProps) => {
       // console.log(error)
     },
   })
+
+  const { mutate: postBlockMutate } = useMutation(
+    () => postBlock(contentList?.pages[0].ownerId ?? -1),
+    {
+      /**
+       *
+       */
+      onSuccess: (response) => {
+        setOwnerNickname(response.nickname)
+        setIsBlockCompleteDialogVisible(true)
+      },
+      /**
+       *
+       */
+      onError: () => {
+        //ignore
+      },
+    }
+  )
 
   /**
    * handleEdit
@@ -222,14 +243,14 @@ const ContentList = ({ route }: ContentListProps) => {
           setIsBlockDialogVisible(false)
         }}
         onClose={(isComplete: boolean) => {
-          isComplete && setIsBlockCompleteDialogVisible(true)
+          isComplete && postBlockMutate()
         }}
       />
       <DefaultDialog
         isVisible={isBlockCompleteDialogVisible}
-        title={i18n.t('blockComplete', { nickname: '다카이브' })}
+        title={i18n.t('blockComplete', { nickname: ownerNickname })}
         imageUrl={defaultImages.blockComplete}
-        description={i18n.t('youCannotSeeBlockUserContents', { nickname: '다카이브' })}
+        description={i18n.t('youCannotSeeBlockUserContents', { nickname: ownerNickname })}
         buttonText="backToCommunity"
         onClick={() => {
           setIsBlockCompleteDialogVisible(false)
