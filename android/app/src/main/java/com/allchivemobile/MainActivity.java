b8e96602191f84ad5764711c9feb6b2c8716685e
Emily <119949731+EmilyPPP@users.noreply.github.com>
2023-08-15 02:00:52 +0900

    feat: add android icons and splash

--- android/app/src/main/java/com/allchivemobile/MainActivity.java
+++ android/app/src/main/java/com/allchivemobile/MainActivity.java
@@ -1,13 +1,19 @@
 package com.allchivemobile;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-
 import com.proyecto26.inappbrowser.RNInAppBrowserModule;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
