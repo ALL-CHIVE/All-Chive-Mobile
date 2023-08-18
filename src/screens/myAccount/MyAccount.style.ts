a1d32f2320b66a607b5ee455bf70a17777176fd9
Emily <119949731+EmilyPPP@users.noreply.github.com>
2023-08-19 02:01:16 +0900

    style: update my account style

--- src/screens/myAccount/MyAccount.style.ts
+++ src/screens/myAccount/MyAccount.style.ts
@@ -66,10 +66,11 @@ export const FooterText = styled.Text`
 export const InfoText = styled.Text`
   ${fonts.body1}
   color: ${colors.gray400};
-  margin-left: 37px;
 `
 
 export const InfoTitle = styled.Text`
-  ${fonts.body1}
-  color: ${colors.mainBlack}
+  ${fonts.body1};
+  color: ${colors.mainBlack};
+  width: 50px;
+  margin-right: 15px;
 `
