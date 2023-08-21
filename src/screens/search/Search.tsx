7424a0f2bd6744212b3796889e2578ff9322948a
Hanna922 <hanna.urssu@gmail.com>
2023-08-21 18:13:19 +0900

    feat: 검색창 뒤로가기 시 검색어 초기화

--- src/screens/search/Search.tsx
+++ src/screens/search/Search.tsx
@@ -138,6 +138,7 @@ const Search = () => {
         <Header>
           <BackButton
             onPress={() => {
+              setSearchText('')
               navigation.goBack()
             }}
           >
