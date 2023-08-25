63dbdf2e08900913b87300ad641b1dddc3de35af
Hanna922 <hanna.urssu@gmail.com>
2023-08-25 19:25:45 +0900

    feat: 사용하지 않는 외부 url 기능 제거

--- src/screens/mypage/components/NavigationList.tsx
+++ src/screens/mypage/components/NavigationList.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { useNavigation } from '@react-navigation/native'
-import { Linking } from 'react-native'
 
 import RightArrowIcon from '@/assets/icons/right_arrow.svg'
 import { MainNavigationProp } from '@/navigations/MainNavigator'
@@ -12,7 +11,6 @@ import { Container, Title } from './NavigationList.style'
 
 interface NavigationListProps {
   title: string
-  url?: string
   openInAppUrl?: string
   screen?:
     | 'MyAccount'
@@ -26,25 +24,13 @@ interface NavigationListProps {
 /**
  * 마이페이지에서 네비게이션 역할을 하는 리스트
  */
-export const NavigationList = ({ title, screen, url, openInAppUrl }: NavigationListProps) => {
+export const NavigationList = ({ title, screen, openInAppUrl }: NavigationListProps) => {
   const navigation = useNavigation<MainNavigationProp>()
   return (
     <>
       {screen && (
         <>
           <Container onPress={() => navigation.navigate(screen)}>
-            <Title>{title}</Title>
-            <RightArrowIcon color={colors.gray500} />
-          </Container>
-        </>
-      )}
-      {url && (
-        <>
-          <Container
-            onPress={() => {
-              Linking.openURL(url)
-            }}
-          >
             <Title>{title}</Title>
             <RightArrowIcon color={colors.gray500} />
           </Container>
